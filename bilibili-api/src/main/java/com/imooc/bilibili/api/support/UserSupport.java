67dfb882a47c80b3c686cebf5791693ec87af1e5
Jianghua Yao <yaojianghua@yaojianghuadeMacBook-Pro.local>
2023-10-27 02:05:09 -0400

    jwt added

---
+++ bilibili-api/src/main/java/com/imooc/bilibili/api/support/UserSupport.java
@@ -0,0 +1,21 @@
+package com.imooc.bilibili.api.support;
+
+import com.imooc.bilibili.service.util.TokenUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class UserSupport {
+
+    public Long getCurrentUserId() throws Exception {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String token = requestAttributes.getRequest().getHeader("token");
+        Long userId = TokenUtil.verifyToken(token);
+        if(userId < 0){
+            throw new Exception("非法用户！");
+        }
+        return userId;
+
+    }
+}
---
+++ bilibili-dao/src/main/java/com/imooc/bilibili/domain/constant/UserConstant.java
@@ -0,0 +1,11 @@
+package com.imooc.bilibili.domain.constant;
+
+public interface UserConstant {
+    public static final String GENDER_MALE = "男";
+    public static final String GENDER_FEMALE = "女";
+    public static final String GENDER_UNKNOWN = "未知";
+
+    public static final String DEFAULT_BIRTH = "1990-10-01";
+
+    public static final String DEFAULT_NICK = "萌新";
+}
